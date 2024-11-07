a59999f1b69a8b230edc82a28545dd040b578568
jinuuukim <kkjjww1503@naver.com>
2024-11-07 21:02:04 +0900

    카테고리필터, 음식데이터 별도관리, 랜덤20개 룰렛에 추가

--- src/Routes/Roulette.js
+++ src/Routes/Roulette.js
@@ -1,76 +1,101 @@
 import { useNavigate } from 'react-router-dom';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../Css/Roulette.module.css';
+import { foods } from '../Data/foods';
 
 function Roulette() {
 	const navigate = useNavigate();
 	const [rotation, setRotation] = useState(0);
+	const [items, setItems] = useState([]);
 	const [selectedItem, setSelectedItem] = useState(null);
-
-	const items = ['초밥', '치킨', '제육', '피자'];
-
-	const handleSpin = () => {
-		const randomDegree = Math.floor(Math.random() * 360 + 1440);
-		const newRotation = rotation + randomDegree;
-		setRotation(newRotation);
-
-		// 회전이 끝난 후 선택된 아이템 계산
-		setTimeout(() => {
-			const totalItems = items.length;
-			const degreesPerItem = 360 / totalItems;
-
-			// 현재 회전 각도를 360으로 나눈 나머지를 구하여 실제 각도 계산
-			const finalRotation = newRotation % 360;
-
-			// 핀의 위치를 고려하여 어떤 아이템이 선택되었는지 계산
-			// 핀의 위치를 중앙에 두기 위해 + degreesPerItem / 4를 추가합니다.
-			const selectedIndex = Math.floor((finalRotation + degreesPerItem / 2) / degreesPerItem) % totalItems;
-			setSelectedItem(items[(totalItems - selectedIndex) % totalItems]); // 반전된 인덱스
-		}, 5000); // 애니메이션 시간과 맞추기 (여기서는 5초로 설정)
+	const [isSpinning, setIsSpinning] = useState(false);
+	const [filteredItems, setFilteredItems] = useState([]);
+  
+	useEffect(() => {
+		filterByCategory('모두');
+	  }, []);
+	
+	const filterByCategory = (category) => {
+		let filtered = foods;
+		if (category !== '모두') {
+		  filtered = foods.filter(food => food.category === category);
+		}
+	const shuffledFoods = shuffleArray(filtered);
+		setFilteredItems(filtered);
+		setItems(shuffledFoods.slice(0, 20));
 	};
+	
+	const shuffleArray = (array) => {
+		return array.sort(() => Math.random() - 0.5);
+	};
+  
+	const handleSpin = () => {
+		if (isSpinning) return;
+		
+		setIsSpinning(true);
 
+	  	const randomDegree = Math.floor(Math.random() * 360 + 2160);
+	  	const newRotation = rotation + randomDegree;
+	  	setRotation(newRotation);
+  
+	 	setTimeout(() => {
+		const totalItems = items.length;
+		const degreesPerItem = 360 / totalItems;
+		const finalRotation = newRotation % 360;
+		const selectedIndex = Math.floor((finalRotation + degreesPerItem / 2) / degreesPerItem) % totalItems;
+		setSelectedItem(items[(totalItems - selectedIndex) % totalItems]);
+		
+		setIsSpinning(false);
+	}, 8000);
+	};
+  
 	return (
-		<div style={{ width: '80%', height: '100vh', margin: '40px' }}>	
-			<div className={styles.roulette_outer}>
-				<div className={styles.roulette_pin}></div>
-				<div className={styles.roulette_outerbtn}>
-					<button className={styles.roulette_btn} onClick={handleSpin}>Spin!</button>
-				</div>
-				<div 
-					className={styles.roulette} 
-					style={{ transform: `rotate(${rotation}deg)` }} 
-				>					
-					<div className={styles.item_wrapper}>
-						{items.map((item, index) => (
-							<div className={styles.item} key={index}>
-								{item}
-							</div>
-						))}
-					</div>
-					<div className={styles.line_wrapper}>
-						<div className={styles.line}></div>		
-						<div className={styles.line}></div>	
-						<div className={styles.line}></div>		
-						<div className={styles.line}></div>						
-					</div>
-				</div>
-			</div>
+	  <div className={styles.container}>
+		<div className={styles.roulette_outer}>
+		  <div className={styles.roulette_pin}></div>
+		  <div className={styles.roulette_outerbtn}>
+			<button className={styles.roulette_btn} onClick={handleSpin}>Spin!</button>
+		  </div>
+		  <div 
+			className={styles.roulette} 
+			style={{ transform: `rotate(${rotation}deg)` }}
+		  >
+            	{items.map((item, index) => (
+                    <div
+                        className={styles.item}
+                        key={index}
+                        style={{ transform: `rotate(${(360 / items.length) * index}deg)` }}
+                    >
+                        <span className={styles.itemName}>{item.name}</span>
+                    </div>
+                ))}
+			  	{[...Array(items.length)].map((_, index) => (
+                    <div
+                        className={styles.line}
+                        key={index}
+                        style={{
+                            transform: `rotate(${(360 / items.length) * index + (360 / items.length) / 2}deg)`
 
-			{selectedItem && ( // 선택된 아이템이 있을 때 결과 표시
-				<div className={styles.selectedItem}>
-					오늘 점심은 {selectedItem} !!
-				</div>
-			)}
-
-			<button
-				onClick={() => {
-					navigate('/map');
-				}}
-			>
-				지도화면으로
-			</button>
+                        }}
+                    />
+            	))}
+		  </div>
+		</div>
+		<div className={styles.category_button}>
+        	<button onClick={() => filterByCategory('모두')}>모두</button>
+        	<button onClick={() => filterByCategory('한식')}>한식</button>
+        	<button onClick={() => filterByCategory('양식')}>양식</button>
+        	<button onClick={() => filterByCategory('중식')}>중식</button>
+        	<button onClick={() => filterByCategory('일식')}>일식</button>
+      	</div>
+		{selectedItem && (
+		  <div className={styles.selectedItem}>
+			오늘 점심은 {selectedItem.name} !!!
+			<button className={styles.nav_Btn} onClick={() => navigate('/map')}>음식점 찾기</button>
+		  </div>)
+		}
 		</div>
 	);
-}
-
-export default Roulette;
+  }
+  
+  export default Roulette;
